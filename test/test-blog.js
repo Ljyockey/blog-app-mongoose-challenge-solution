5b80b50d1b613a1995f16ad509e2e1bd9dee22b1
ljyockey <linyockey@gmail.com>
2017-05-03 07:50:25 -0700

    added before/after functions and first GET test

--- test/test-blog.js
+++ test/test-blog.js
@@ -8,24 +8,69 @@ const {app, runServer, closeServer} = require('../server');
 const {BlogPost} = require('../models');
 
 const should = chai.should();
-chai.use('chaiHttp');
+chai.use(chaiHttp);
 
 function seedBlogData() {
 	console.info('seeding blog data');
 	const seedData = [];
 	for (let i=1; i<= 10; i++) {
-		seedData.push(generateBlogData);
+		seedData.push(generateBlogData());
 	}
 	return BlogPost.insertMany(seedData);
 }
 
 function generateBlogData() {
 	return {
-		title: faker.Lorem.words().join(" "),
+		title: faker.lorem.word(),
 		author: {
-			firstName: faker.firstName(),
-			lastName: faker.lastName()
+			firstName: faker.name.firstName(),
+			lastName: faker.name.lastName()
 		},
-		content: faker.Lorem.sentence()
+		content: faker.lorem.sentence()
 	};
 }
+
+function tearDownDb() {
+	console.warn('Deleting database');
+	return mongoose.connection.dropDatabase();
+}
+
+describe('Blog API Resources', function() {
+
+before(function() {
+	return runServer(TEST_DATABASE_URL);
+});
+
+beforeEach(function() {
+	return seedBlogData();
+});
+
+afterEach(function() {
+	return tearDownDb();
+});
+
+after(function() {
+	return closeServer();
+});
+
+describe ('GET endpoint', function() {
+
+	it('should return list of all existing blog posts', function() {
+		let res;
+		return chai.request(app)
+		.get('/posts')
+		.then(function(_res) {
+			res = _res;
+			res.should.have.status(200);
+			res.body.should.have.length.of.at.least(1);
+			return BlogPost.count();
+		})
+		.then(function(count) {
+			res.body.should.have.length.of(count);
+		});
+	});
+
+	// it('should return restaurants with right fields');
+});
+
+});
